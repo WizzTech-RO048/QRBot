99415664412e42562e39e0eab39316d4baa78e5f
VladTomici14 <vladtomici@gmail.com>
2021-10-19 11:06:51 +0300

    tested editing the repo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+// testing
 public class Robot {
     private DcMotor leftFront, leftRear, rightFront, rightRear;
 
