74dc05530f5e3c713dc9376cc89b0c5a526d479e
WizzTechTM <78214456+WizzTechTM@users.noreply.github.com>
2021-10-20 12:31:06 +0300

    Create FuckingViewPort.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FuckingViewPort.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.icu.util.Output;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
+
+import org.firstinspires.ftc.robotcore.internal.opmode.OpModeManagerImpl;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+
+import java.io.OutputStream;
+
+public class FuckingViewPort {
+    private LinearLayout viewportContainerLayout;
+    private SurfaceView view;
+
+    public FuckingViewPort(int containerLayoutId) {
+        setupViewport(containerLayoutId);
+
+        // AppUtil.getInstance().getApplication().registerComponentCallbacks();
+        // OpModeManagerImpl.getOpModeManagerOfActivity(AppUtil.getInstance().getActivity()).registerListener();
+    }
+
+    private void setupViewport(final int containerLayoutId) {
+        AppUtil.getInstance().getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    viewportContainerLayout = (LinearLayout) AppUtil.getInstance().getActivity().findViewById(containerLayoutId);
+
+                    if(viewportContainerLayout == null) {
+                        throw new Exception("Viewport container specified by user does not exist");
+                    } else if(viewportContainerLayout.getChildCount() != 0) {
+                        throw new Exception("Viewport container specified by user is not empty!");
+                    }
+
+                    view = new SurfaceView(AppUtil.getInstance().getActivity());
+                    viewportContainerLayout.setVisibility(View.VISIBLE);
+                    viewportContainerLayout.addView(view);
+                } catch (Exception e) { e.printStackTrace(); }
+            }
+        });
+    }
+
+    public void draw(Bitmap currentBitmap)
+    {
+        while (true)
+        {
+            Canvas canvas = view.getHolder().lockCanvas();
+
+            if (canvas != null)
+            {
+                if(currentBitmap != null)
+                {
+                    Paint paint = new Paint();
+
+                    canvas.drawBitmap(currentBitmap, 0, 0, paint);
+
+                    currentBitmap.recycle();
+                }
+            }
+        }
+    }
+}
