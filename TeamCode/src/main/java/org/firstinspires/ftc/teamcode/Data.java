bcc6ba8eb5d3c6f25d30d043d18fa527c0833066
VladTomici14 <vladtomici@gmail.com>
2021-10-21 18:36:28 +0300

    edited the files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Data.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Data.java
@@ -3,13 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
 @TeleOp(name = "Data")
 public class Data extends OpMode{
     private Controller controller;
-
-    Telemetry telemetry;
 
     @Override
     public void init(){
@@ -20,6 +16,7 @@ public class Data extends OpMode{
     public void loop(){
         controller.update();
 
+        telemetry.clear()
         telemetry.addData("x: ", controller.leftStickX);
         telemetry.addData("y: ", controller.leftStickY);
         telemetry.update();
