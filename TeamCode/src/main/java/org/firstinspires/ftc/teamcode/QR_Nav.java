c53cf42073f6ba53f18e7dc766a6f276dcdc7cc4
VladTomici14 <vladtomici@gmail.com>
2021-10-27 23:06:59 +0300

    simplified the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QR_Nav.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/QR_Nav.java
@@ -7,7 +7,10 @@ import android.os.Handler;
 import androidx.annotation.NonNull;
 
 import com.google.zxing.BinaryBitmap;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.FormatException;
 import com.google.zxing.LuminanceSource;
+import com.google.zxing.NotFoundException;
 import com.google.zxing.RGBLuminanceSource;
 import com.google.zxing.Reader;
 import com.google.zxing.ResultPoint;
@@ -20,12 +23,15 @@ import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.android.util.Size;
+import org.firstinspires.ftc.robotcore.external.function.Consumer;
 import org.firstinspires.ftc.robotcore.external.function.Continuation;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraCaptureRequest;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraCaptureSequenceId;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraCaptureSession;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraCharacteristics;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraException;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraFrame;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraManager;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.internal.collections.EvictingBlockingQueue;
@@ -55,9 +61,6 @@ public class QR_Nav extends LinearOpMode {
     private FuckingViewPort webcamViewport;
 
     private Robot robot;
-
-    private String lastInstruction = null;
-    private final Reader qrCodesReader = new QRCodeReader();
 
     private int centerX, centerY;
     private static final double MOVE_SPEED = 0.5;
@@ -93,6 +96,7 @@ public class QR_Nav extends LinearOpMode {
 
             robot.move(0, MOVE_SPEED);
 
+            // for the FuckingViewport class
             int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
             webcamViewport = new FuckingViewPort(cameraMonitorViewId);
 
@@ -118,60 +122,59 @@ public class QR_Nav extends LinearOpMode {
     private void onNewFrame(Bitmap frame) {
         String instruction = readQRCode(frame);
 
-        if (instruction == null) {
-            return;
-        }
+        if (instruction != null) {
+            telemetry.addData("Instruction", instruction);
+            telemetry.addData("Turbo", robot.isTurbo());
+            // telemetry.addData("heading:", robot.getHeading());
+            telemetry.update();
 
-        telemetry.addData("Instruction", instruction);
-        telemetry.addData("Turbo", robot.isTurbo());
-        // telemetry.addData("heading:", robot.getHeading());
-        telemetry.update();
+            switch (instruction) {
+                // in current circumstances, we are going to use case "2" as "forward" and case "1" as "backward"
+                case "1":
 
-        switch (instruction) {
-            // in current circumstances, we are going to use case "2" as "forward" and case "1" as "backward"
-            case "1":
+                    break;   // move backward
+                case "2":
+                    robot.blockingRotate(90, MOVE_SPEED);
+                    robot.move(0, MOVE_SPEED);
 
-                break;   // move backward
-            case "2":
-                robot.blockingRotate(90, MOVE_SPEED);
-                robot.move(0, MOVE_SPEED);
+                    break;  // move forward
+                case "3":
+                    robot.blockingRotate(180, MOVE_SPEED);
+                    robot.move(0, MOVE_SPEED);
 
-                break;  // move forward
-            case "3":
-                robot.blockingRotate(180, MOVE_SPEED);
-                robot.move(0, MOVE_SPEED);
+                    break;   // move right
+                case "4":
+                    robot.blockingRotate(90, MOVE_SPEED);
+                    robot.move(0, MOVE_SPEED);
 
-                break;   // move right
-            case "4":
-                robot.blockingRotate(90, MOVE_SPEED);
-                robot.move(0, MOVE_SPEED);
+                    break;  // move left
+                case "5":
+                    robot.blockingRotate(270, MOVE_SPEED);
+                    robot.move(0, MOVE_SPEED);
 
-                break;  // move left
-            case "5":
-                robot.blockingRotate(270, MOVE_SPEED);
-                robot.move(0, MOVE_SPEED);
+                    break;  // rotate right
+                case "6":
+                    robot.blockingRotate(65, MOVE_SPEED);
+                    robot.move(0, -MOVE_SPEED);
 
-                break;  // rotate right
-            case "6":
-                robot.blockingRotate(65, MOVE_SPEED);
-                robot.move(0, -MOVE_SPEED);
+                    try {
+                        Thread.sleep(300);
+                    } catch (Exception ignored) {
+                    }
 
-                try {
-                    Thread.sleep(300);
-                } catch (Exception ignored) {}
+                    robot.stop();
+                    robot.move(0, MOVE_SPEED);
 
-                robot.stop();
-                robot.move(0, MOVE_SPEED);
+                    break;  // rotate left
+                case "7":
 
-                break;  // rotate left
-            case "7":
+                    robot.goCrazy();
 
-                robot.goCrazy();
+                    break; // cut the rope
+                case "8":
 
-                break; // cut the rope
-            case "8":
-
-                break;
+                    break;
+            }
         }
     }
 
@@ -180,7 +183,12 @@ public class QR_Nav extends LinearOpMode {
     // -------------------
     private void initFrameQueue(int capacity) {
         frameQueue = new EvictingBlockingQueue<>(new ArrayBlockingQueue<>(capacity));
-        frameQueue.setEvictAction(Bitmap::recycle);
+        frameQueue.setEvictAction(new Consumer<Bitmap>() {
+            @Override
+            public void accept(Bitmap frame) {
+                frame.recycle();
+            }
+        });
     }
 
     private void openCamera() {
@@ -201,10 +209,9 @@ public class QR_Nav extends LinearOpMode {
         final int imageFormat = ImageFormat.YUY2;
         CameraCharacteristics camCharacteristics = cameraName.getCameraCharacteristics();
 
-        if (Arrays.stream(camCharacteristics.getAndroidFormats()).noneMatch(v -> v == imageFormat)) {
+        if(!contains(camCharacteristics.getAndroidFormats(), imageFormat)) {
             telemetry.addData("ERROR: ", "image format not supported");
             telemetry.update();
-            return;
         }
 
         final Size size = camCharacteristics.getDefaultSize(imageFormat);
@@ -216,17 +223,20 @@ public class QR_Nav extends LinearOpMode {
                 public void onConfigured(@NonNull CameraCaptureSession session) {
                     try {
                         final CameraCaptureRequest captureRequest = camera.createCaptureRequest(imageFormat, size, fps);
-                        session.startCapture(
-                                captureRequest,
-                                (session1, request, cameraFrame) -> {
-                                    Bitmap bmp = captureRequest.createEmptyBitmap();
-                                    cameraFrame.copyToBitmap(bmp);
-                                    frameQueue.offer(bmp);
-                                },
-                                Continuation.create(callbackHandler, (session12, cameraCaptureSequenceId, lastFrameNumber) -> {
-                                    RobotLog.ii("QR Test", "capture sequence %s reports completed: lastFrame=%d", cameraCaptureSequenceId, lastFrameNumber);
-                                })
-                        );
+                        session.startCapture(captureRequest, new CameraCaptureSession.CaptureCallback() {
+                            @Override
+                            public void onNewFrame(@NonNull CameraCaptureSession session, @NonNull CameraCaptureRequest request, @NonNull CameraFrame cameraFrame) {
+                                Bitmap bmp = captureRequest.createEmptyBitmap();
+                                cameraFrame.copyToBitmap(bmp);
+                                frameQueue.offer(bmp);
+                            }
+                        }, Continuation.create(callbackHandler, new CameraCaptureSession.StatusCallback() {
+                            @Override
+                            public void onCaptureSequenceCompleted(@NonNull CameraCaptureSession session, CameraCaptureSequenceId cameraCaptureSequenceId, long lastFrameNumber) {
+                                RobotLog.ii("QR Test", "capture sequence %s reports completed: lastFrame=%d", cameraCaptureSequenceId, lastFrameNumber);
+                            }
+                        }));
+
                         synchronizer.finish(session);
                     } catch (CameraException | RuntimeException e) {
                         RobotLog.ee("QR Test", e, "exception starting capture");
@@ -246,55 +256,65 @@ public class QR_Nav extends LinearOpMode {
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-
         cameraCaptureSession = synchronizer.getValue();
     }
 
     private void stopCamera() {
-        if (cameraCaptureSession == null) {
-            return;
+        if(cameraCaptureSession != null) {
+            cameraCaptureSession.stopCapture();
+            cameraCaptureSession.close();
+            cameraCaptureSession = null;
         }
-
-        cameraCaptureSession.stopCapture();
-        cameraCaptureSession.close();
-        cameraCaptureSession = null;
     }
 
     private void closeCamera() {
-        if (camera == null) {
-            return;
-        }
-
         stopCamera();
-        camera.close();
-        camera = null;
+        if(camera != null) {
+            camera.close();
+            camera = null;
+        }
     }
 
     // -------------------
     //       Utils
     // -------------------
     public String readQRCode(Bitmap bitmap) {
+        String decoded = null;
         int[] pixelsArray = new int[bitmap.getWidth() * bitmap.getHeight()];
         bitmap.getPixels(pixelsArray, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
         LuminanceSource source = new RGBLuminanceSource(bitmap.getWidth(), bitmap.getHeight(), pixelsArray);
         BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source));
+        Reader qrCodesReader = new QRCodeReader();
 
         centerX = bitmap.getWidth() / 2;
         centerY = bitmap.getHeight() / 2;
-        
+
         try {
+            // printing the (x, y) coordinatas of the QR code
             ResultPoint[] resultPoints = qrCodesReader.decode(binaryBitmap).getResultPoints();
             int sizeOfResultPoint = resultPoints.length;
-
-            for(int i = 0; i < sizeOfResultPoint; i++){
+            for (int i = 0; i < sizeOfResultPoint; i++) {
                 String formatted = String.format("X: %f, Y: %f", resultPoints[i].getX(), resultPoints[i].getY());
                 telemetry.addData(String.format("Point #%d", i), formatted);
                 telemetry.update();
             }
-            return qrCodesReader.decode(binaryBitmap).getText();
-        } catch (Exception e) {
+
+            // returning the string of the QR code
+            decoded = qrCodesReader.decode(binaryBitmap).getText();
+        } catch (NotFoundException e) {
             e.printStackTrace();
-            return null;
+        } catch (ChecksumException e) {
+            e.printStackTrace();
+        } catch (FormatException e) {
+            e.printStackTrace();
         }
+        return decoded;
+    }
+
+    private boolean contains(int[] array, int value){
+        for(int i : array){
+            if(i == value){ return true; }
+        }
+        return false;
     }
 }
