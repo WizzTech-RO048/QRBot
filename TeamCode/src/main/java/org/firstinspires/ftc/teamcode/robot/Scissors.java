83760ad46d48497d64e949099151a461599beafa
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-11-05 18:13:13 +0200

    Inline params, implement cut functionality, add docs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Scissors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Scissors.java
@@ -4,39 +4,104 @@ import androidx.annotation.NonNull;
 import com.qualcomm.robotcore.hardware.*;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+import java.util.Objects;
 import java.util.concurrent.*;
 
 public class Scissors {
-    private final Parameters params;
+    private final DcMotorEx arm;
+    private final Servo scissors;
+    private final ScheduledExecutorService scheduler;
+    private final Telemetry telemetry;
+    private final int armRaisedPosition;
 
     Scissors(@NonNull final Parameters params) {
-        params.arm.setDirection(DcMotorSimple.Direction.REVERSE);
-        params.arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        params.arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        arm = Objects.requireNonNull(params.arm, "Scissors arm is not set");
+        arm.setDirection(DcMotorSimple.Direction.REVERSE);
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        this.params = params;
+        scissors = Objects.requireNonNull(params.scissors, "Scissors servo is not set");
+        scissors.setDirection(Servo.Direction.REVERSE);
+
+        scheduler = Objects.requireNonNull(params.scheduler, "Scheduler is not set");
+        telemetry = Objects.requireNonNull(params.telemetry, "Telemetry is not set");
+        armRaisedPosition = params.armRaisedPosition;
     }
 
-    private ScheduledFuture<?> lastMove = null;
+    private ScheduledFuture<?> lastMove = null, lastCut = null;
 
+    /**
+     * Raise or lower the scissors' arm. Note that the initial position of the arm
+     * is considered the zero position - make sure the arm is positioned down before
+     * starting the OpMode!
+     *
+     * @param positionPercentage Number in the interval [0.0, 1.0] that represents
+     *                           how much the arm should be raised.
+     * @return A future that completes when the arm is raised. Canceling this future
+     * will stop the arm from moving.
+     */
     public ScheduledFuture<?> moveArm(double positionPercentage) {
-        if (lastMove != null && !lastMove.isDone() && !lastMove.cancel(true)) {
+        if (!Utils.isDone(lastMove) && !lastMove.cancel(true)) {
+            telemetry.addLine("Last move not done!");
             return null;
         }
 
-        int targetPosition = (int) Math.floor(Utils.interpolate(0, params.armRaisedPosition, positionPercentage, 1));
-        int initialPosition = params.arm.getCurrentPosition();
+        int targetPosition = (int) Math.floor(Utils.interpolate(0, armRaisedPosition, positionPercentage, 1));
+        int initialPosition = arm.getCurrentPosition();
         if (targetPosition == initialPosition) {
             return null;
         }
 
-        params.arm.setTargetPosition(targetPosition);
-        params.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        params.arm.setPower(targetPosition > initialPosition ? 1 : -1);
+        arm.setTargetPosition(targetPosition);
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        arm.setPower(targetPosition > initialPosition ? 1 : -1);
 
-        lastMove = Utils.poll(params.scheduler, () -> !params.arm.isBusy(), () -> params.arm.setPower(0), 10, TimeUnit.MILLISECONDS);
+        lastMove = Utils.poll(scheduler, () -> !arm.isBusy(), () -> arm.setPower(0), 10, TimeUnit.MILLISECONDS);
 
         return lastMove;
+    }
+
+    /**
+     * Set the arm's motor power.
+     *
+     * Use this function to move the arm in its zero position, then restart the OpMode.
+     *
+     * @param power The desired power.
+     */
+    public void move(double power) {
+        arm.setPower(power);
+    }
+
+    private static final double SCISSORS_POS = 0.3;
+
+    /**
+     * Do a cut.
+     *
+     * @return A future that completes when the scissors are back in their initial position.
+     */
+    public ScheduledFuture<?> cut() {
+        if (!Utils.isDone(lastCut) && !lastCut.cancel(true)) {
+            return null;
+        }
+
+        Servo s = scissors;
+        s.setPosition(SCISSORS_POS);
+
+        lastCut = scheduler.schedule(() -> s.setPosition(0), 500, TimeUnit.MILLISECONDS);
+
+        return lastCut;
+    }
+
+    /**
+     * Revert the scissors back to their initial position. Call this function if you want to
+     * cancel a cut.
+     */
+    public void cancelCut() {
+        if (Utils.isDone(lastCut) || !lastCut.cancel(true)) {
+            return;
+        }
+
+        scissors.setPosition(0);
     }
 
     static class Parameters {
